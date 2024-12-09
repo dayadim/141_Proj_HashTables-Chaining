74e24b06f6cd0341860da19c7b213682c819fad1
Ahmad <ahmad139@mail.fresnostate.edu>
2024-12-08 23:25:24 -0800

    Double hash to main

--- main.cc
+++ main.cc
@@ -1,6 +1,8 @@
 #include "hash.h"
 #include "LP_hash.h"
 #include "LL_hash.h"
+#include "2x_hash.h"
+#include "BST_hash.h"
 #include <random>
 #include <chrono>
 using namespace std;
@@ -45,10 +47,75 @@ void linear_probing() {
 	cerr << "Not implemented.\n";
 }
 
-void double_hashing() {
-	cout << "==================== TIMING FOR DOUBLE HASHING ====================" << endl;
-	
-	cerr << "Not implemented.\n";
+void two_step_hashing() {
+    cout << "==================== TIMING FOR DOUBLE HASHING ====================" << endl;
+
+    vector<HashDataType> elements = gen_elements(size_ * 10, 141);
+    Twox_hash<HashDataType, size_>* table = new Twox_hash<HashDataType, size_>;
+
+    vector<double> average_insert_timings;
+    vector<double> average_search_timings;
+
+    int capacity = 0;
+    int total_capacity = size_ * .4; // 40% capacity
+    int time_increment = size_ / 10; // 10% increments
+
+    while (capacity < size_ * 10) {
+        int phase_end = capacity + total_capacity;
+        for (int i = capacity; i < phase_end; ++i) {
+            table->insert(elements[i]);
+        }
+        capacity = phase_end;
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<int> dis(0, size_ - 1);
+
+        auto search_start_time = high_resolution_clock::now();
+        for (int i = 0; i < time_increment; ++i) {
+            table->search(elements[dis(gen)]);
+        }
+        auto search_end_time = high_resolution_clock::now();
+
+        double total_search_duration = duration_cast<nanoseconds>(search_end_time - search_start_time).count();
+        double average_search_time = total_search_duration / time_increment;
+        average_search_timings.push_back(average_search_time);
+
+        cout << "(" << ((capacity) * 100.0 / size_) << "%): Average search time = "
+             << average_search_time << " ns/search.\n";
+
+        int timed_insert_start = capacity;
+        int timed_insert_end = capacity + time_increment;
+
+        auto insert_start_time = high_resolution_clock::now();
+        for (int i = timed_insert_start; i < timed_insert_end; ++i) {
+            table->insert(elements[i]);
+        }
+        auto insert_end_time = high_resolution_clock::now();
+
+        double total_insert_duration = duration_cast<nanoseconds>(insert_end_time - insert_start_time).count();
+        double average_insert_time = total_insert_duration / time_increment;
+        average_insert_timings.push_back(average_insert_time);
+
+        cout << "(" << (capacity * 100.0 / size_) << "% to "
+             << ((capacity + time_increment) * 100.0 / size_) << "%): Average insert time = "
+             << average_insert_time << " ns/insert.\n";
+
+        capacity += time_increment;
+    }
+
+    delete table;
+
+    cout << "\nFinal Average Timings:\n";
+    cout << "Insertion (avg. ns/insert):\n";
+    for (double timing : average_insert_timings) {
+        cout << timing << "\n";
+    }
+
+    cout << "\nSearch (avg. ns/search):\n";
+    for (double timing : average_search_timings) {
+        cout << timing << "\n";
+    }
 }
 
 void BST_chaining() {
@@ -225,12 +292,12 @@ void ll_chaining() {
 	}
 }
 
-void bst_chaining() {
+/* void bst_cha vining() {
 	cout << "==================== TIMING FOR BST CHAINING ====================" << endl;
 	
 	cerr << "Not implemented.\n";
-}
-
+} 
+*/
 int main() {
 	int choice;
 	cout << "Select an option:\n";
@@ -245,13 +312,13 @@ int main() {
 			linear_probing();
 			break;
 		case 2:
-			double_hashing();
+			two_step_hashing();
 			break;
 		case 3:
 			ll_chaining();
 			break;
 		case 4:
-			bst_chaining();
+			BST_chaining();
 			break;
 		default:
 			cout << "Invalid choice" << endl;
